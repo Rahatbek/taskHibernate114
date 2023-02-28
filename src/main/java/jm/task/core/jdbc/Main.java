0dcec276b0eb466b24ad96fa17bc78982afad97c
rkojantaev <kojantaev2992@gmail.com>
2023-03-01 02:35:58 +0600

    finished task 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,70 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/users", "rahat", "mysql");
+                 Statement statement = conn.createStatement();) {
+                statement.executeUpdate("create table if not exists user(id bigint not null AUTO_INCREMENT PRIMARY KEY, name varchar(255), lastName varchar(255), age tinyint)");
+
+                statement.execute("insert into user(name, lastName, age) values('Rahat', 'Kozhantaev', 30)");
+                ResultSet rs = statement.executeQuery("select name from users.user where name = 'Rahat'");
+                User user = new User();
+                while (rs.next()) {
+                    user.setName(rs.getString("name"));
+                    System.out.println("1-user: " + user.getName());
+                }
+
+                statement.execute("insert into user(name, lastName, age) values('Roma', 'Romanov', 26)");
+                rs = statement.executeQuery("select name from users.user where name = 'Roma'");
+                while (rs.next()) {
+                    user.setName(rs.getString("name"));
+                    System.out.println("2-user: " + user.getName());
+                }
+
+                statement.execute("insert into user(name, lastName, age) values('Viktor', 'Viktorov', 28)");
+                rs = statement.executeQuery("select name from users.user where name = 'Viktor'");
+                while (rs.next()) {
+                    user.setName(rs.getString("name"));
+                    System.out.println("3-user: " + user.getName());
+                }
+
+                statement.execute("insert into user(name, lastName, age) values('Anton', 'Antonov', 24)");
+                rs = statement.executeQuery("select name from users.user where name = 'Anton'");
+                while (rs.next()) {
+                    user.setName(rs.getString("name"));
+                    System.out.println("4-user: " + user.getName());
+                }
+
+                List<User> users = new ArrayList<>();
+                rs = statement.executeQuery("select * from user");
+                while (rs.next()) {
+                    User userForList = new User();
+                    userForList.setId(rs.getLong("id"));
+                    userForList.setName(rs.getString("name"));
+                    userForList.setLastName(rs.getString("lastName"));
+                    userForList.setAge(rs.getByte("age"));
+                    users.add(userForList);
+                }
+                System.out.println("Получение всех: " + users);
+
+                statement.executeUpdate("delete from users.user");
+
+                statement.executeUpdate("drop table if exists user");
+            }
+        } catch (Exception ex) {
+            System.out.println("Connection failed...");
+
+            System.out.println(ex);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,12 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -12,31 +17,102 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            String sql = "create table if not exists user(id bigint not null AUTO_INCREMENT PRIMARY KEY, name varchar(255), lastName varchar(255), age tinyint)";
+            session.beginTransaction();
+            NativeQuery sqlQuery = session.createSQLQuery(sql);
+            sqlQuery.executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("when create table: " + sqlQuery);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            String sql = "drop table if exists user";
 
+            session.beginTransaction();
+            NativeQuery sqlQuery = session.createSQLQuery(sql);
+            sqlQuery.executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("when drop table: " + sqlQuery);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
 
+            NativeQuery insertsqlQuery = session.createSQLQuery("INSERT INTO user(name,lastName,age) VALUES (?,?,?)");
+
+            insertsqlQuery.setParameter(1, name);
+            insertsqlQuery.setParameter(2, lastName);
+            insertsqlQuery.setParameter(3, age);
+            insertsqlQuery.executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("inserted");
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
 
+            NativeQuery insertsqlQuery = session.createSQLQuery("delete from user where id = ?");
+
+            insertsqlQuery.setParameter(1, id);
+            insertsqlQuery.executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("deleted");
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> resultList = new ArrayList<>();
+        try (Session session = Util.getSessionFactory().openSession()) {
+
+            session.beginTransaction();
+            NativeQuery query = session.createSQLQuery("select * from user");
+            List<Object[]> rows = query.list();
+            for (Object[] row : rows) {
+                User user = new User();
+                user.setId(Long.parseLong(row[0].toString()));
+                user.setName(row[1].toString());
+                user.setLastName(row[2].toString());
+                user.setAge(Byte.parseByte(row[3].toString()));
+                resultList.add(user);
+            }
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        System.out.println(resultList);
+        return resultList;
     }
 
     @Override
     public void cleanUsersTable() {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            String sql = "delete from users.user";
 
+            session.beginTransaction();
+            NativeQuery sqlQuery = session.createSQLQuery(sql);
+            sqlQuery.executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("deleted table: " + sqlQuery);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 }
